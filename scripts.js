de352488319254f22f5d933e7e911351f1894b82
Achilisnard <achil.isnard@gmail.com>
2025-05-05 18:56:54 +0200

    yml update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
+
 let currentSlide = 0;
 const slides = document.querySelectorAll(".carousel img");
-
+slides.forEach((slide, index) => {
+  slide.style.display = index === 0 ? "block" : "none";
+});
 function changeSlide(n) {
   slides[currentSlide].style.display = "none";
   currentSlide = (currentSlide + n + slides.length) % slides.length;
@@ -16,5 +19,21 @@ function loadTextFromFile(filePath, elementId) {
     });
 }
 
+async function chargerYAML() {
+  const response = await fetch('data.yml');
+  const yamlText = await response.text();
+
+  const data = jsyaml.load(yamlText);
+
+  document.getElementById('titre').textContent = data.titre;
+  document.getElementById('lieu').textContent = data.lieu;
+  document.getElementById('contacte').textContent = data.contacte;
+  document.getElementById('prix').textContent = data.prix;
+  ifram = document.getElementById('map_id');
+  ifram.src = data.geolocalisation;
+
+}
+
+chargerYAML();
 // Load text for the "A propos" section
 loadTextFromFile("text/a-propos.txt", "a-propos");
